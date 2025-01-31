b98058322271e3922d4019f742c68bdc194ce480
Ghale Abduallah <ghale001.wrok@gmail.com>
2025-01-31 16:07:33 +0300

    add some styles

--- lib/core/style/theme.dart
+++ lib/core/style/theme.dart
@@ -2,23 +2,44 @@ import 'package:e_commerce/core/style/colors.dart';
 import 'package:flutter/material.dart';
 
 appTheme() => ThemeData(
-      colorScheme: ColorScheme.light(
-        primary: AppColors.primary,
-        onPrimary: AppColors.onPrimary,
-        inversePrimary: AppColors.inversePrimary,
+    colorScheme: const ColorScheme.light(
+      primary: AppColors.primary,
+      onPrimary: AppColors.onPrimary,
+      inversePrimary: AppColors.inversePrimary,
+    ),
+    appBarTheme: const AppBarTheme(
+      foregroundColor: Color(0xff1f293d),
+      backgroundColor: Colors.white,
+      titleTextStyle: TextStyle(
+          fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xff1f293d)),
+    ),
+    scaffoldBackgroundColor: const Color(0xfff5f5f5),
+    textTheme: const TextTheme(
+        displaySmall: TextStyle(fontSize: 26),
+        titleLarge: TextStyle(
+            fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black54),
+        bodySmall: TextStyle(fontSize: 14, color: Colors.grey)),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all(AppColors.primary),
+        foregroundColor: WidgetStateProperty.all(AppColors.onPrimary),
       ),
-      appBarTheme: AppBarTheme(
-        foregroundColor: Color(0xff1f293d),
-        backgroundColor: Colors.white,
-        titleTextStyle: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Color(0xff1f293d)),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(color: Colors.grey),
       ),
-      scaffoldBackgroundColor: Color(0xfff5f5f5),
-      textTheme: TextTheme(
-          displaySmall: TextStyle(fontSize: 26),
-          titleLarge: TextStyle(
-              fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black54),
-          bodySmall: const TextStyle(fontSize: 14, color: Colors.grey)),
-    );
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(color: AppColors.primary),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(color: Colors.redAccent),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(color: Colors.redAccent),
+      ),
+    ));
